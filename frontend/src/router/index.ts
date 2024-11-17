e3f52374a5a538b8a8c48cba5f6a36e12de2a17f
pumpkin <wkghd951223@gmail.com>
2024-11-17 15:40:12 +0900

    feat made index page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SeoulView from '../views/SeoulView.vue';
 import TokyoView from '../views/TokyoView.vue';
+import DashboardView from '../views/DashboardView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', name: 'dashboard', component: DashboardView },
     { path: '/seoul', name: 'seoul', component: SeoulView },
     { path: '/tokyo', name: 'tokyo', component: TokyoView },
   ],
---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<template setup>
+  <h1>Make it snowflakes</h1>
+</template>
+  
+<script setup lang="ts">
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  padding-top: 150px;
+}
+</style>
